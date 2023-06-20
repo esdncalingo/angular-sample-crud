115770b6114827c97bc213c2b7eaf3ee17589353
esdncalingo <esdncalingo@gmail.com>
2023-06-20 13:05:26 +0800

    sample CRUD

--- src/app/components/dataview/dataview.component.ts
+++ src/app/components/dataview/dataview.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { getUsers } from 'src/app/services/api/user_api';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { getUsers, createUsers, deleteUsers, updateUsers } from 'src/app/services/api/user_api';
 
 @Component({
   selector: 'dataview',
@@ -8,14 +9,75 @@ import { getUsers } from 'src/app/services/api/user_api';
 })
 export class DataViewComponent {
   users: any[] = [];
+  myForm: FormGroup;
+  editingID!: number;
 
-  constructor() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
+    this.myForm = formBuilder.group({
+      name: '',
+      email: '',
+      phone: '',
+      address: ''
+    })
+  }
 
   async ngOnInit() {
     try {
       this.users = await getUsers();
     } catch (error) {
       console.log(error)
+    }
+  }
+
+  async addUser() {
+    console.log('User Added', this.myForm.value)
+    try {
+      const data = await createUsers(this.myForm.value)
+      console.log(data)
+      this.users.push(this.myForm.value)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async deleteUsers(id: number) {
+    console.log('User Deleted', id)
+    try {
+      const data = await deleteUsers(id)
+      this.users = this.users.filter(user => user.id !== id)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  editUser(id: number) {
+    console.log('Editing User', id)
+    const user_info = this.users.find(user => user.id == id)
+    this.myForm.patchValue(user_info)
+    this.editingID = id
+    console.log(user_info)
+  }
+
+  async updateUser() {
+    if (this.editingID !== 0) {
+      console.log('User Updated', this.editingID)
+      const data = await updateUsers(this.editingID, this.myForm.value)
+      const updatedUserIndex = this.users.findIndex(user => user.id === this.editingID);
+        if (updatedUserIndex !== -1) {
+          this.users[updatedUserIndex] = { ...this.users[updatedUserIndex], ...this.myForm.value };
+        }
+      this.myForm.patchValue({
+        name: '',
+        email: '',
+        phone: '',
+        address: ''
+      })
+      this.editingID = 0
+    } else {
+      console.log('No Selected User')
     }
   }
 }
--- src/app/services/api/user_api.ts
+++ src/app/services/api/user_api.ts
@@ -11,3 +11,30 @@ export const getUsers = async () => {
     return error.response.data
   }
 }
+
+export const createUsers = async (params: any) => {
+  try {
+    const res = await axios.post(`${BASE_URL}/users`, params)
+    return res.data
+  } catch (error: any) {
+    return error.response.data
+  }
+}
+
+export const deleteUsers = async (id: number) => {
+  try {
+    const res = await axios.delete(`${BASE_URL}/users/${id}`)
+    return res.data
+  } catch (error: any) {
+    return error.response.data
+  }
+}
+
+export const updateUsers = async (id: number, params: any) => {
+  try {
+    const res = await axios.put(`${BASE_URL}/users/${id}`, params)
+    return res.data
+  } catch (error: any) {
+    return error.response.data
+  }
+}
